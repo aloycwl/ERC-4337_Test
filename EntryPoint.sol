075b7e51584b7489c01d624cc60d7e8cf136141d
aloycwl <aloycw@gmail.com>
2023-03-14 14:13:58 +0800

    Removed unncessary functions

--- EntryPoint.sol
+++ EntryPoint.sol
@@ -1,7 +1,210 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 
-import"Interfaces.sol";
-import"Helpers.sol";
+struct UserOperation{
+    address sender;uint nonce;bytes initCode;bytes callData;uint callGasLimit;uint verificationGasLimit;
+    uint preVerificationGas;uint maxFeePerGas;uint maxPriorityFeePerGas;bytes paymasterAndData;bytes signature;
+}
+library UserOperationLib {
+    function getSender(UserOperation calldata userOp)internal pure returns(address){
+        address data;
+        assembly {data:=calldataload(userOp)}
+        return address(uint160(data));
+    }
+    function gasPrice(UserOperation calldata userOp)internal view returns(uint){unchecked{
+        (uint maxFeePerGas,uint maxPriorityFeePerGas)=(userOp.maxFeePerGas,userOp.maxPriorityFeePerGas);
+        return maxFeePerGas==maxPriorityFeePerGas?maxFeePerGas:min(maxFeePerGas,maxPriorityFeePerGas+block.basefee);
+    }}
+    function pack(UserOperation calldata userOp)internal pure returns(bytes memory ret){
+        bytes calldata sig=userOp.signature;
+        assembly{
+            let ofs:=userOp
+            let len:=sub(sub(sig.offset,ofs),32)
+            ret:=mload(0x40)
+            mstore(0x40,add(ret,add(len,32)))
+            mstore(ret,len)
+            calldatacopy(add(ret,32),ofs,len)
+        }
+    }
+    function hash(UserOperation calldata userOp)internal pure returns(bytes32) {
+        return keccak256(pack(userOp));
+    }
+    function min(uint a,uint b)internal pure returns(uint){
+        return a<b?a:b;
+    }
+}
+
+interface IPaymaster{
+    enum PostOpMode{opSucceeded,opReverted,postOpReverted}
+    function validatePaymasterUserOp(UserOperation calldata,bytes32,uint)
+    external returns(bytes memory,uint);
+    function postOp(PostOpMode,bytes calldata,uint)external;
+}
+interface IAccount{
+    function validateUserOp(UserOperation calldata,bytes32,uint)
+    external returns(uint);
+}
+interface IStakeManager {
+    event Deposited(address,uint);
+    event Withdrawn(address,address,uint);
+    event StakeLocked(address,uint,uint);
+    event StakeUnlocked(address,uint);
+    event StakeWithdrawn(address,address,uint);
+    struct DepositInfo{uint deposit;bool staked;uint stake;uint32 unstakeDelaySec;uint48 withdrawTime;}
+    struct StakeInfo {uint stake;uint unstakeDelaySec;}
+    function getDepositInfo(address)external view returns(DepositInfo memory);
+    function balanceOf(address)external view returns(uint);
+    function depositTo(address)external payable;
+    function addStake(uint32)external payable;
+    function unlockStake()external;
+    function withdrawStake(address payable)external;
+    function withdrawTo(address payable,uint) external;
+}
+interface IEntryPoint is IStakeManager{
+    event UserOperationEvent(bytes32 ,address ,address,uint,bool,uint,uint);
+    event AccountDeployed(bytes32,address,address,address);
+    event UserOperationRevertReason(bytes32,address,uint,bytes);
+    event SignatureAggregatorChanged(address);
+    error FailedOp(uint,string);
+    error SignatureValidationFailed(address);
+    error ValidationResult(ReturnInfo,StakeInfo,StakeInfo,StakeInfo);
+    error ValidationResultWithAggregation(ReturnInfo,StakeInfo,StakeInfo,StakeInfo,AggregatorStakeInfo);
+    error SenderAddressResult(address);
+    error ExecutionResult(uint,uint,uint48,uint48,bool,bytes);
+    struct UserOpsPerAggregator{UserOperation[]userOps;IAggregator aggregator;bytes signature;}
+    struct ReturnInfo{uint preOpGas;uint prefund;bool sigFailed;uint48 validAfter;uint48 validUntil;bytes paymasterContext;}
+    struct AggregatorStakeInfo{address aggregator;StakeInfo stakeInfo;}
+    function handleOps(UserOperation[]calldata,address payable)external;
+    function handleAggregatedOps(UserOpsPerAggregator[]calldata,address payable)external;
+    function getUserOpHash(UserOperation calldata)external view returns(bytes32);
+    function simulateValidation(UserOperation calldata)external;
+    function getSenderAddress(bytes memory initCode)external;
+    function simulateHandleOp(UserOperation calldata, address,bytes calldata)external;
+}
+interface IAggregator{
+    function validateSignatures(UserOperation[]calldata,bytes calldata)external view;
+    function validateUserOpSignature(UserOperation calldata)external view returns(bytes memory);
+    function aggregateSignatures(UserOperation[]calldata)external view returns(bytes memory);
+}
+
+library Exec{
+    function call(address to,uint value,bytes memory data,uint txGas)internal returns(bool success){
+        assembly{
+            success:=call(txGas,to,value,add(data,0x20),mload(data),0,0)
+        }
+    }
+    function staticcall(address to,bytes memory data,uint txGas)internal view returns(bool success){
+        assembly{
+            success:=staticcall(txGas,to,add(data,0x20),mload(data),0,0)
+        }
+    }
+    function delegateCall(address to,bytes memory data,uint txGas)internal returns(bool success){
+        assembly{
+            success:=delegatecall(txGas,to,add(data,0x20),mload(data),0,0)
+        }
+    }
+    function getReturnData(uint maxLen) internal pure returns(bytes memory returnData){
+        assembly{
+            let len:=returndatasize()
+            if gt(len,maxLen){
+                len:=maxLen
+            }
+            let ptr:=mload(0x40)
+            mstore(0x40,add(ptr,add(len,0x20)))
+            mstore(ptr,len)
+            returndatacopy(add(ptr,0x20),0,len)
+            returnData:=ptr
+        }
+    }
+    function revertWithData(bytes memory returnData)internal pure{
+        assembly{
+            revert(add(returnData,32),mload(returnData))
+        }
+    }
+    function callAndRevert(address to,bytes memory data,uint maxLen)internal{
+        if(!call(to,0,data,gasleft()))revertWithData(getReturnData(maxLen));
+    }
+}
+struct ValidationData{
+    address aggregator;
+    uint48 validAfter;
+    uint48 validUntil;
+}
+function _parseValidationData(uint validationData)pure returns(ValidationData memory data){
+    uint48 validUntil=uint48(validationData>>160);
+    if (validUntil==0)validUntil=type(uint48).max;
+    return ValidationData(address(uint160(validationData)),uint48(validationData>>208),validUntil);
+}
+function _intersectTimeRange(uint256 validationData, uint256 paymasterValidationData) pure returns (ValidationData memory){
+    (ValidationData memory accountValidationData,ValidationData memory pmValidationData)=
+    (_parseValidationData(validationData),_parseValidationData(paymasterValidationData));
+    address aggregator=accountValidationData.aggregator;
+    if(aggregator==address(0))aggregator=pmValidationData.aggregator;
+    (uint48 validAfter,uint48 validUntil,uint48 pmValidAfter,uint48 pmValidUntil)=
+    (accountValidationData.validAfter,accountValidationData.validUntil,pmValidationData.validAfter,pmValidationData.validUntil);
+    if(validAfter < pmValidAfter)validAfter=pmValidAfter;
+    if(validUntil > pmValidUntil)validUntil=pmValidUntil;
+    return ValidationData(aggregator,validAfter,validUntil);
+}
+function _packValidationData(ValidationData memory data)pure returns(uint256){
+    return uint160(data.aggregator)|(uint256(data.validUntil)<<160)|(uint256(data.validAfter)<<208);
+}
+function _packValidationData(bool sigFailed,uint48 validUntil,uint48 validAfter)pure returns(uint256){
+    return(sigFailed?1:0)|(uint256(validUntil)<<160)|(uint256(validAfter)<<208);
+}
+
+contract SenderCreator {
+    function createSender(bytes calldata initCode)external returns(address sender){
+        (address factory,bytes memory initCallData)=(address(bytes20(initCode[0:20])),initCode[20:]);
+        bool success;
+        assembly{
+            success:=call(gas(),factory,0,add(initCallData,0x20),mload(initCallData),0,32)
+            sender:=mload(0)
+        }
+        if(!success)sender=address(0); 
+    }
+}
+
+abstract contract StakeManager is IStakeManager{
+    mapping(address=>DepositInfo)public deposits;
+    function getDepositInfo(address account)public view returns(DepositInfo memory){
+        return deposits[account];
+    }
+    function _getStakeInfo(address addr)internal view returns(StakeInfo memory info) {
+        DepositInfo storage depositInfo=deposits[addr];
+        (info.stake,info.unstakeDelaySec)=(depositInfo.stake,depositInfo.unstakeDelaySec);
+    }
+    function balanceOf(address account)public view returns(uint){
+        return deposits[account].deposit;
+    }
+    receive() external payable{
+        depositTo(msg.sender);
+    }
+    function _incrementDeposit(address account,uint amount)internal{
+        deposits[account].deposit+=amount;
+    }
+    function depositTo(address account)public payable{
+        _incrementDeposit(account,msg.value);
+    }
+    function addStake(uint32 unstakeDelaySec)public payable{
+        deposits[msg.sender]=DepositInfo(deposits[msg.sender].deposit,true,deposits[msg.sender].stake+msg.value,unstakeDelaySec,0);
+    }
+    function unlockStake()external{
+        DepositInfo storage info=deposits[msg.sender];
+        (info.withdrawTime,info.staked)=(uint48(block.timestamp)+info.unstakeDelaySec,false);
+    }
+    function withdrawStake(address payable withdrawAddress)external{
+        DepositInfo storage info=deposits[msg.sender];
+        (info.unstakeDelaySec,info.withdrawTime,info.stake)=(0,0,0);
+        (bool success,)=withdrawAddress.call{value:info.stake}("");
+        require(success);
+    }
+    function withdrawTo(address payable withdrawAddress,uint withdrawAmount)external{
+        DepositInfo storage info=deposits[msg.sender];
+        info.deposit=info.deposit-withdrawAmount;
+        (bool success,)=withdrawAddress.call{value:withdrawAmount}("");
+        require(success);
+    }
+}
 
 contract EntryPoint is IEntryPoint,StakeManager{
     using UserOperationLib for UserOperation;
